b4d56215eeed588856f80e2d83b9034922e19d20
13020180064 <61999159+13020180064@users.noreply.github.com>
2020-03-16 08:11:54 -0700

    Add files via upload

---
+++ liaa/liaa.java
@@ -0,0 +1,18 @@
+/*   Nama         :Rulia
+     Kelas        :B1
+     Waktu        :22.01
+     Hari/Tanggal :senin/16 maret 2020
+*/
+public class liaa{
+      public static void main(String[] args){
+      String Nim           ="13020180064";
+      String Nama          ="Rulia";
+      String Jurusan       ="Teknik Informatika";
+      String Fakultas      ="Ilmu Komputer";
+      System.out.println("Nim saya : "+ Nim);
+      System.out.println("Nama saya : "+ Nama);
+      System.out.println("Jurusan saya : "+ Jurusan);
+      System.out.println("Fakultas saya :"+ Fakultas);
+}
+}
+     
